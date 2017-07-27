aa1504112829b3b616e02393fe960e01eb084c90
Alexey Qian <Alexey.Qian@catapultsystems.com>
2017-07-26 20:19:38 -0500

    update classes of search page

--- client/components/HotelItemComponent.js
+++ client/components/HotelItemComponent.js
@@ -14,18 +14,24 @@ class HotelItemComponent extends React.Component {
       <div id={'hotel_'+hotel.id} className="hotelItem">
 
         <img src={hotel.photo} />
-        <h3>{hotel.name}</h3>
-        <div><span className="label label-default">7</span> Good</div>
+        <h4>{hotel.name}</h4>
+
         <div>
           <span className="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
           <span>{hotel.address}</span>
         </div>
-        <div>
-          <span className="label label-default">Parking</span>
-          <span className="label label-default">Wifi</span>
+
+        <div className="facility">
+
+          <span className="label label-default">7</span><span>Good</span>
+          <i className="fa fa-car" aria-hidden="true"></i><span>Parking</span>
+          <i className="fa fa-wifi" aria-hidden="true"></i><span>Wifi</span>
+          <i className="fa fa-car" aria-hidden="true"></i><span>Bar</span>
+
         </div>
 
-        <div><span>{hotel.currency_symbol}{hotel.price}</span></div>
+        <div className="price">{hotel.currency_symbol}{hotel.price}</div>
+        <div className="booknow"><button className="btn btn-warning">Book Now</button></div>
 
       </div>
 
