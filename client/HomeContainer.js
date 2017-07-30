199b9feeeb50a203cc03c1023be5cb6c3af4d4fc
AlexeyQian <alexeyqian@gmail.com>
2017-07-29 22:39:12 -0500

    use sass to organize css files

--- client/HomeContainer.js
+++ client/HomeContainer.js
@@ -2,4 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import HomeSearchComponent from './components/HomeSearchComponent';
 
+import './sass/site.scss';
+
 render(<HomeSearchComponent/>, document.getElementById('homeSearchComponent'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,96 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var BUILD_DIR = path.resolve(__dirname, 'public/js');
-var APP_DIR = path.resolve(__dirname, 'client');
+const APP_DIR = path.resolve(__dirname, 'client');
+const BUILD_DIR = path.resolve(__dirname, 'public');
 
 var config = {
     entry: {
         home: [APP_DIR + '/HomeContainer.js'],
         search: [APP_DIR + '/SearchContainer.js'],
         hotel: [APP_DIR + '/HotelContainer.js'],
-        //search: [APP_DIR + '/CityAndDate.js', APP_DIR + '/FilterButtonMobileContainer.js'],
-
-        //adminDashboard: [APP_DIR + '/AdminRLEmailLeadInfoContainer.jsx']
+        //search: [APP_DIR + '/CityAndDate.js', APP_DIR + '/FilterButtonMobileContainer.js']
     },
     output: {
         path: BUILD_DIR,
-        filename: '[name].js'
+        filename: 'js/[name].js'
     },
     module: {
-        loaders: [
+        rules: [
           {
               test: /\.js$/,
-              include: APP_DIR,
-              loader: 'babel-loader'
-          },
-          {
-              test: /\.jsx?/,
-              include: APP_DIR,
-              loader: 'babel-loader'
-          },
-          {
-              test: /\.css$/,
-              loader: 'style-loader'
-          },
-          {
-              test: /\.css$/,
-              loader: 'css-loader',
-              query: {
-                  modules: true,
-                  localIdentName: '[name]__[local]___[hash:base64:5]'
+              loader: "babel-loader",
+              options: {
+                presets: [["es2015", {modules: false}], "react"]
               }
-          }
+          },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: [
+                { loader: "css-loader",  options: {sourceMap: true}},
+                { loader: "sass-loader", options: {sourceMap: true}}
+              ]
+            })
+          },
+          // {
+          //   test: /\.scss$/,
+          //   use: [
+          //     {
+          //       loader: "style-loader" // creates style nodes from JS strings
+          //     },
+          //     {
+          //       loader: "css-loader", // translates CSS into CommonJS
+          //       options: {
+          //           sourceMap: true
+          //       }
+          //     },
+          //     {
+          //       loader: "sass-loader", // compiles Sass to CSS
+          //       options: {
+          //           sourceMap: true
+          //       }
+          //       // options: {
+          //       //     includePaths: ["absolute/path/a", "absolute/path/b"]
+          //       // }
+          //     }
+          //   ]
+          // },
+          // {
+          //     test: /\.css$/,
+          //     loader: 'style-loader'
+          // },
+          // {
+          //     test: /\.css$/,
+          //     loader: 'css-loader',
+          //     query: {
+          //         modules: true,
+          //         localIdentName: '[name]__[local]___[hash:base64:5]'
+          //     }
+          // },
+          // {
+          //   test: /\.(png|jpg|gif)$/,
+          //   loader: "file-loader?name=img/img-[hash:6].[ext]"
+          // },
+          // {
+          //   test: /\.(png|jpg|gif)$/,
+          //   loader: "url-loader?limit=5000&name=img/img-[hash:6].[ext]"
+          // },
+          // {
+          //   test: /\.html$/,
+          //   loader: "html-loader"
+          // },
+          // {
+          //   test: require.resolve("lodash"),
+          //   loader: 'expose?_'
+          // }
         ]
-    }
+    },
+    plugins: [
+      new ExtractTextPlugin("css/site.css"),
+    ]
 };
 
 module.exports = config;
