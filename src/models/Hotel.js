ca4049ce2600a27a3fc68a8143192bd1311b0658
Qian <Alexey.Qian@tea.texas.gov>
2017-08-17 16:47:06 -0500

    update hotel and room models

--- src/models/Hotel.js
+++ src/models/Hotel.js
@@ -3,14 +3,59 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var hotelSchema = new Schema({
-	hotel_no: {type: String, required: true},
-	//lost_date: {type: Date, required: true},
-	zipcode: String,
-	location: String,
+	fid: {type: String, required: true},
+  name: {type: String, required: true},
 
-	description: String,
+  price_min: Number,
+  price_max: Number,
+  price_currency: String,
 
-	facilities: Array,
+  addr_street1: String,
+  addr_street2: String,
+  addr_district: String,
+  addr_city: {type: String, required: true},
+  addr_state: {type: String, required: true},
+  addr_zipcode: {type: String, required: true},
+  ddr_country: {type: String, required: true},
+  addr_formated: String,
+  latitude: Number,
+  longitude: Number,
+
+  photo_default: String,
+  photos: [Schema.Types.Mixed],
+
+  description: String,
+	telephone: String,
+
+  stars: Number,
+  chain: String,
+  category: String,
+  themes: [String],
+
+  facilities: Array,
+  room_facilities:Array,
+
+  policies: {
+    checkin: String,
+    checkout: String,
+    free_cancellation: Boolean,
+		cancellation_rule: String,
+		no_payment_needed: Boolean,
+		pay_at: String,
+    children_and_extra_bed: String,
+    pets_allowed: Boolean,
+    accepted_credit_cards: Array,
+  },
+
+  other_info: String,
+
+  distances: Array,
+
+  revew_count: Number,
+  review_score: Number,
+  location_score: Number,
+  favorited_count: Number,
+  recommended_count: Number,
 
 	created_by: {type: String, required: true},
 	created_at: Date,
--- src/models/Room.js
+++ src/models/Room.js
@@ -3,10 +3,39 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var roomSchema = new Schema({
-	room_no: {type: String, required: true},
+	fid: {type: String, required: true},
+	room_type_id: Schema.Types.ObjectId,
+	room_type_fid: {type: String, required: true},
+  hotel_id: Schema.Types.ObjectId,
+  hotel_fid: String,
+  name: String,
 
+  price_currency: String,
+  price: Number,
 
+  sqft: Number,
+
+  bed_type: String,
+  bed_count: Number,
+  can_add_bed: Boolean,
+
+  include_breakfast: Boolean,
+  free_wifi: Boolean,
+  free_cancellation: Boolean,
+	cancellation_rule: String,
+	no_payment_needed: Boolean,
+	pay_at: String,
+
+  fits: Number,
+  floor: String,
+  photo_default: String,
+  photos:[Schema.Types.Mixed],
+  facilities: [String],
+
+  display_order: Number,
 	description: String,
+	other_info: Schema.Types.Mixed,
+
 	created_by: {type: String, required: true},
 	created_at: Date,
 	updated_at: Date
