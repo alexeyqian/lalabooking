ae2f3c751a3921a4c047ca1b032b8d56a8211f28
Alexey <alexeyqian@gmail.com>
2021-01-05 09:02:14 +0700

    Update common.js
    remove expired file

--- config/env/common.js
+++ config/env/common.js
@@ -1,8 +1,4 @@
 module.exports = {
   port: process.env.PORT || 5000,
-  database: 'mongodb://alexey:Paw%40123'
-  	+ '@studycluster-shard-00-00-14ucg.mongodb.net:27017'
-  	+ ',studycluster-shard-00-01-14ucg.mongodb.net:27017'
-  	+ ',studycluster-shard-00-02-14ucg.mongodb.net:27017'
-  	+ '/findpaw?ssl=true&replicaSet=StudyCluster-shard-0&authSource=admin'
+  database: ''
 }
